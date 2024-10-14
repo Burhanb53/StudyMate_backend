d0d23ffd83123a27ae9ca6131fb7a4726fbdc240
Burhanb53 <burhanuddinb542@gmail.com>
2024-10-14 13:22:12 +0530

    update connection string

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://Burhanb53:b60400056@studymate.ewgmi.mongodb.net/studymate");
-    console.log("MongoDB connected successfully");
+    await mongoose.connect(
+      "mongodb+srv://Burhanb53:b60400056@studymate.ewgmi.mongodb.net/studymate?retryWrites=true&w=majority",
+      
+    );
+    console.log("MongoDB connected...");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
